b0795798698fa74e9881384c9aade4b05cbbc692
du-honglei <ouc2025@stu.ouc.edu.cn>
2026-08-25 15:02:49 +0800

    Update Goods.h

--- Goods.h
+++ Goods.h
@@ -1,1 +1,16 @@
+#ifndef GOODS_H
+#define GOODS_H
 
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Goods {
+	public:
+		Goods();
+	private:
+		int goodid;
+		string goodname;
+    string gooddesc[50];
+};
